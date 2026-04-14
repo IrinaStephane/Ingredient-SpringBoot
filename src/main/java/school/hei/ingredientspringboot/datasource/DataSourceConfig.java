84f461369f2e0eadad9845dd73dc765badaa0f9e
Irina <irinarakoto2005@gmail.com>
2026-04-14 15:50:08 +0300

    feat(api): add OpenAPI specification (openapi.yml) for dishes and ingredients endpoints

--- src/main/java/school/hei/ingredientspringboot/datasource/DataSourceConfig.java
+++ src/main/java/school/hei/ingredientspringboot/datasource/DataSourceConfig.java
@@ -2,20 +2,23 @@ package school.hei.ingredientspringboot.datasource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 public class DataSourceConfig {
+    private final String url = System.getenv("JDBC_URL");
+    private final String username = System.getenv("USERNAME");
+    private final String password = System.getenv("PASSWORD");
 
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(System.getenv("JDBC_URL"));
-        dataSource.setUsername(System.getenv("USERNAME"));
-        dataSource.setPassword(System.getenv("PASSWORD"));
-        return dataSource;
+    public Connection getConnection(){
+        try {
+            return DriverManager.getConnection(url,username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
